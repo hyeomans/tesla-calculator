22de5e9800826d90dcac633646ce83874787c157
Hector Yeomans <yeomans.leonel@gmail.com>
2017-10-27 07:16:01 -0700

    Adding TeslaCar component 1.7

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import './TeslaBattery.css';
+import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
+import TeslaCar from '../components/TeslaCar/TeslaCar';
 
 export default class TeslaBattery extends Component {
   render() {
     return (
       <form className="tesla-battery">
         <h1>Range per Change</h1>
+        <TeslaCar wheelSize={19}/>
+        <TeslaNotice />
       </form>
     )
   }
