7bf400329291298b876d85a91e88318b061498ca
Hector Yeomans <yeomans.leonel@gmail.com>
2017-10-27 06:36:05 -0700

    Finished adding TeslaBattery 1.5

---
+++ src/containers/TeslaBattery.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './TeslaBattery.css';
+
+export default class TeslaBattery extends Component {
+  render() {
+    return (
+      <form className="tesla-battery">
+        <h1>Range per Change</h1>
+      </form>
+    )
+  }
+}
